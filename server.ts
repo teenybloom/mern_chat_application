d77250225c108506e760397f75496118bed91be2
Mosuswalks <mosuswalks@gmail.com>
2019-02-27 17:57:45 -0500

    Bugs and Issues

--- server.ts
+++ server.ts
@@ -1,3 +1,14 @@
 const koa = require('koa');
-const router = require('koa-router');
-const 
+const koaRouter = require('koa-router');
+require('dotenv').config();
+
+const app = new koa();
+
+const router = new koaRouter();
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
+
+
+
+app.listen(process.env.PORT, () => console.log(`Server Started on Port: ${process.env.PORT}`))
